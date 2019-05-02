61651f5d7eb938c2974ffbf88f1f5c1550cfdb17
이혜윤 (HyeYoon Lee) <48234805+201820028hy@users.noreply.github.com>
2019-05-02 12:34:52 +0900

    Add files via upload

--- TableViewSection03/ViewController.swift
+++ TableViewSection03/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var myTableView: UITableView!
     var num = ["0", "1", "2", "3", "4"]
@@ -18,7 +18,8 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return num.count
+        //return num.count
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -26,7 +27,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         let row = indexPath.row
         
         cell.textLabel?.text = "Section : \(indexPath.section)"
-        cell.detailTextLabel?.text = "Row : \(num[row])"
+        cell.detailTextLabel?.text = "Row : \(row)"
         
         return cell
     }
@@ -35,7 +36,16 @@ class ViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         myTableView.dataSource = self
+        myTableView.delegate = self
     }
-
+    
+    //Delegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0//row 높이 100.0만큼 높아짐
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row = \(indexPath.row) Section = \(indexPath.section)")
+    }
 
 }
